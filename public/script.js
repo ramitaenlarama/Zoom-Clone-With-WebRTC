f048fda5ece914de0d3764b123aa072b1e1f2963
Zimanra <ramanzincristian@gmail.com>
2020-08-13 01:00:15 -0300

    cambio puerto 3001

--- public/script.js
+++ public/script.js
@@ -1,9 +1,8 @@
 const socket = io('/')
 const videoGrid = document.getElementById('video-grid')
 const myPeer = new Peer(undefined, {
-  host: 'call-zoom-ramita.herokuapp.com',
-  port: '443',
-  secure: true
+  host: 'https://call-zoom-ramita.herokuapp.com/',
+  port: '3001'
 })
 const myVideo = document.createElement('video')
 myVideo.muted = true
